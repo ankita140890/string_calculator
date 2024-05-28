536adac0a674d3504591481745ef77763e508c6d
Ankita Agrawal <ankita.agrawal1414@gmail.com>
2024-05-28 15:14:09 +0530

    Functionality added to ignore numbers greater than 1000 in string for addition

--- app/services/string_calc.rb
+++ app/services/string_calc.rb
@@ -10,6 +10,7 @@ class StringCalc
     end
 
     number_list = numbers.split(/#{delimiter}/).map(&:to_i)
+    number_list.select! { |num| num <= 1000 } # Remove numbers larger than 1000
     negatives = number_list.select { |n| n < 0 }
     raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
 
