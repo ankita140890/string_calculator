0056e17fb65b5852b43d62aaa8ee98f9d38ad7ed
Ankita Agrawal <ankita.agrawal1414@gmail.com>
2024-05-28 17:39:25 +0530

    Functionality added to allow delimiters of any length with a specified format.

--- app/services/string_calc.rb
+++ app/services/string_calc.rb
@@ -5,16 +5,16 @@ class StringCalc
     delimiter = ",|\n"
     if numbers.start_with?("//")
       parts = numbers.split("\n", 2)
-      delimiter = Regexp.escape(parts.first[2..-1])
+
+      delimiter = Regexp.escape(parts.first[2..-1]).split('').map { |char| "\\" + char }.join("|")
       numbers = parts.last
     end
 
     number_list = numbers.split(/#{delimiter}/).map(&:to_i)
-    number_list.select! { |num| num <= 1000 } # Remove numbers larger than 1000
+    number_list.reject! { |num| num > 1000 } # Ignore numbers larger than 1000
     negatives = number_list.select { |n| n < 0 }
     raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
 
     number_list.sum
-
   end
 end
